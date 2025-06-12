96d2674ee11a7113d1621b4154d8d914e317a584
filip <vertigo.filip@gmaol.com>
2025-06-12 19:39:31 +0200

    added wifi

--- app/src/main/kotlin/org/fossify/paint/activities/SplashActivity.kt
+++ app/src/main/kotlin/org/fossify/paint/activities/SplashActivity.kt
@@ -1,9 +1,33 @@
 package org.fossify.paint.activities
-
 import android.content.Intent
 import org.fossify.commons.activities.BaseSplashActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+
 
 class SplashActivity : BaseSplashActivity() {
+
+    private val bluetoothPermissions = arrayOf(
+        Manifest.permission.BLUETOOTH,
+        Manifest.permission.BLUETOOTH_ADMIN,
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.BLUETOOTH_SCAN,
+        Manifest.permission.BLUETOOTH_CONNECT
+    )
+
+    private fun checkAndRequestPermissions() {
+        val missing = bluetoothPermissions.filter {
+            ContextCompat.checkSelfPermission(this, it) != PackageManager.PERMISSION_GRANTED
+        }
+
+        if (missing.isNotEmpty()) {
+            ActivityCompat.requestPermissions(this, missing.toTypedArray(), 1)
+        }
+    }
+
     override fun initActivity() {
         startActivity(Intent(this, MainActivity::class.java))
         finish()
